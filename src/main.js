9e2ffbc22dfb87ca5ffbd861f7efa5ae53a57d58
Tejiri-A <tjamrasa8@gmail.com>
2025-04-13 10:14:06 +0100

    style: implement mobile navigation styles and responsive navigation toggle with open/close buttons and animations

--- src/main.js
+++ src/main.js
@@ -1,2 +1,15 @@
 'use strict'
 import './sass/main.scss'
+
+// NAV TOGGLE
+const btnOpen = document.getElementById('nav__open')
+const btnClose = document.getElementById('nav__close')
+const navList = document.querySelector('.nav__list')
+
+btnOpen.addEventListener('click', () => {
+    navList.classList.add('show__nav')
+})
+
+btnClose.addEventListener('click', () => {
+    navList.classList.remove('show__nav')
+})
